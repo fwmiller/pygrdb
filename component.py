6f06e78e2d10c1cececf7bd2e6e44fb0fc3d3fbc
Frank W. Miller <frmi5636@colorado.edu>
2018-05-19 12:57:51 -0700

    Graph and component print routines

--- component.py
+++ component.py
@@ -1,11 +1,16 @@
 import os
 from config import GRDB_DIR
 
+def component_command(argv):
+	components_print()
+
 def components_print():
+	clist = []
 	rdir = os.path.expanduser(GRDB_DIR)
 	gdirs = os.listdir(rdir)
 	for gdir in gdirs:
-		print('Graph', gdir)
 		cdirs = os.listdir(rdir + '/' + gdir)
 		for cdir in cdirs:
-			print(cdir)
+			clist.append(''.join(gdir + '.' + cdir))
+	for c in sorted(clist):
+		print(c)
--- graph.py
+++ graph.py
@@ -1,8 +1,17 @@
 import os
 from config import GRDB_DIR
 
+def graph_command(argv):
+	graphs_print()
+
 def graphs_print():
-	rootdir = os.path.expanduser(GRDB_DIR)
-	dirs = os.listdir(rootdir)
+	rdir = os.path.expanduser(GRDB_DIR)
+	dirs = os.listdir(rdir)
+	glist = []
 	for dir in dirs:
-		print(dir)
+		glist.extend(dir)
+	for g in sorted(glist):
+		print(g)
+
+def graph_new():
+	return
