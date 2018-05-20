8d19ec4982959312d8b130d9b411f7adeaff0c4c
Frank W. Miller <frmi5636@colorado.edu>
2018-05-20 10:58:35 -0700

    Added component_new() function

--- component.py
+++ component.py
@@ -1,7 +1,13 @@
 import os
 from config import GRDB_DIR
 
-def component_command(argv):
+def component_command(argv, gno):
+	if len(argv) > 1:
+		if argv[1] == 'new' or argv[1] == 'n':
+			component_new(gno)
+		return
+
+	# Dump all components if no arguments
 	components_print()
 
 def components_get_list(gno):
@@ -22,3 +28,14 @@ def components_print():
 			clist.append(''.join(gdir + '.' + cdir))
 	for c in sorted(clist):
 		print(c)
+
+def component_new(gno):
+	# Find highest existing component number for specified graph and
+	# add one
+	clist = components_get_list(gno)
+	cnolist = list(map(int, clist))
+	cno = max(cnolist or (-1)) + 1
+
+	# Create directory for new component
+	gdir = os.path.expanduser(GRDB_DIR) + '/' + str(gno)
+	os.mkdir(gdir + '/' + str(cno))
