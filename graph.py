b3350dc4514ebd60978d75ed595345cb88ed0b0a
Frank W. Miller <frmi5636@colorado.edu>
2018-05-20 09:00:07 -0700

    Added graph_new() function

--- graph.py
+++ graph.py
@@ -2,16 +2,36 @@ import os
 from config import GRDB_DIR
 
 def graph_command(argv):
-	graphs_print()
+	if len(argv) > 1:
+		if argv[1] == 'new' or argv[1] == 'n':
+			graph_new()
+		return
 
-def graphs_print():
+	# Dump all graphs if no arguments
+	graphs_print()
+	
+def graphs_get_list():
+	glist = []
 	rdir = os.path.expanduser(GRDB_DIR)
 	dirs = os.listdir(rdir)
-	glist = []
 	for dir in dirs:
-		glist.extend(dir)
+		glist.append(dir)
+	return glist
+
+def graphs_print():
+	glist = graphs_get_list()
 	for g in sorted(glist):
 		print(g)
 
 def graph_new():
-	return
+	# Find highest existing graph number and add one
+	glist = graphs_get_list()
+	gnolist = list(map(int, glist))
+	gno = max(gnolist) + 1
+
+	# Create directory for new graph
+	rdir = os.path.expanduser(GRDB_DIR)
+	os.mkdir(rdir + '/' + str(gno))
+
+	# Create first component directory for new graph
+	os.mkdir(rdir + '/' + str(gno) + '/0')
