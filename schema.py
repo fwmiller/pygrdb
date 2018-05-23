8e90bb8dc2bbd21302b7b54831ce13730ce6396e
Frank W. Miller <frank@frankwmiller.net>
2018-05-23 15:17:29 -0700

    More work on schemas

--- schema.py
+++ schema.py
@@ -2,37 +2,52 @@ import json
 import os
 from config import *
 
-def schema_read(gno, cno, schema_type):
-	# Check whether a schema exists
-	cdir = os.path.expanduser(GRDB_DIR) + '/' + str(gno) + '/' + str(cno)
-	if schema_type == 'vertex':
-		schemafilename = cdir + '/' + VERTEX_SCHEMA_FILE
-
-	elif schema_type == 'edge':
-		schemafilename = cdir + '/' + EDGE_SCHEMA_FILE
-
+def schema_gen_filename(gno, cno, schema_type):
+	fname = os.path.expanduser(GRDB_DIR)
+	fname += '/' + str(gno)
+	fname += '/' + str(cno)
+	if schema_type == 'vertex' or schema_type == 'v':
+		fname += '/' + VERTEX_SCHEMA_FILE
+	elif schema_type == 'edge' or schema_type == 'e':
+		fname += '/' + EDGE_SCHEMA_FILE
 	else:
 		return None
+	return fname
 
-	# Read in the JSON schema to a dictionary version
+def schema_read(gno, cno, schema_type):
+	schemafilename = schema_gen_filename(gno, cno, schema_type)
+	if not schemafilename:
+		return None
 	try:
-		schemafile = open(schemafilename)
+		f = open(schemafilename)
 	except:
 		return None
 
-	schemastr = schemafile.read()
-	schema = json.loads(schemastr)
-
+	schema = json.loads(f.read())
+	f.close()
 	return schema
 
-def schema_write(schema, gno, cno):
-	return
+
+def schema_write(schema, gno, cno, schema_type):
+	schemafilename = schema_gen_filename(gno, cno, schema_type)
+	if not schemafilename:
+		return
+
+	print('Write schema', schema)
+
+	f = open(schemafilename, 'w+')
+	json.dump(schema, f)
+	f.close()
+
 
 def schema_print(gno, cno, schema_type):
 	schema = schema_read(gno, cno, schema_type)
+	print(schema)
+
 
 def schema_new():
 	return {}
+
 
 def schema_add(schema, name, base_type):
 	if not schema:
