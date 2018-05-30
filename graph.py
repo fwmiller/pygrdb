422bdee11490aac36ff00b646417e6bfd61d9366
Frank W. Miller <frmi5636@colorado.edu>
2018-05-29 21:59:48 -0700

    More work on graph_print()

--- graph.py
+++ graph.py
@@ -15,14 +15,51 @@ def graph_print(gidx, cidx):
 	rdir = os.path.expanduser(GRDB_DIR)
 	gdir = rdir + '/' + gidx
 	cdir = gdir + '/' + cidx
+
+	# Print vertex set
 	vfile = cdir + '/' + VERTEX_FILE
 	try:
-		vfd = open(vfile, 'r')
+		vfd = open(vfile, 'rb')
 	except:
+		print('Open', vfile, 'failed')
 		return
 
 	# XXX Assume no tuples for the moment
+	print('{', end='')
+	vidbytes = vfd.read(8)
+	while vidbytes:
+		vid = int.from_bytes(vidbytes, byteorder=sys.byteorder)
+		print(str(vid), end='')
+		vidbytes = vfd.read(8)
+		if not vidbytes:
+			break;
+		print(',', end='')
+	print('}', end='')
+	vfd.close()
 
+	# Print edge set
+	efile = cdir + '/' + VERTEX_FILE
+	try:
+		efd = open(efile, 'rb')
+	except:
+		print('Open', efile, 'failed')
+		return
+
+	# XXX Assume no tuples for the moment
+	print(',{', end='')
+	eid1bytes = efd.read(8)
+	eid2bytes = efd.read(8)
+	while eid1bytes and eid2bytes:
+		eid1 = int.from_bytes(eid1bytes, byteorder=sys.byteorder)
+		eid2 = int.from_bytes(eid2bytes, byteorder=sys.byteorder)
+		print('(' + str(eid1) + ',' + str(eid2) + ')', end='')
+		eid1bytes = efd.read(8)
+		eid2bytes = efd.read(8)
+		if not eid1bytes or eid2bytes:
+			break;
+		print(',', end='')
+	print('}', end='')
+	efd.close()
 
 def graphs_print():
 	#
