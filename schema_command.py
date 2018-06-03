57cf7a1e08e789b9054a90fece3f7d62f91c0978
Frank W. Miller <frmi5636@colorado.edu>
2018-06-02 20:57:21 -0700

    Added ability to add edge schemas

--- schema_command.py
+++ schema_command.py
@@ -1,29 +1,30 @@
 from schema import *
 
-def schema_command_vertex(argv, gno, cno):
+def schema_command_add(argv, gno, cno):
 	if len(argv) != 4:
-		print('Illegal number of arguments for schema vertex add')
+		print('Illegal number of arguments for schema add')
 		return
 
 	if argv[1] == 'vertex' or argv[1] == 'v':
-		# Read existing vertex schema
 		schema = schema_read(gno, cno, 'v')
-
-		# Add new attribute to vertex schema
 		schema = schema_add(schema, argv[2], argv[3])
-
-		# Write new vertex schema
 		schema_write(gno, cno, 'v', schema)
+
+	elif argv[1] == 'edge' or argv[1] == 'e':
+		schema = schema_read(gno, cno, 'e')
+		schema = schema_add(schema, argv[2], argv[3])
+		schema_write(gno, cno, 'e', schema)
 
 
 def schema_command(argv, gno, cno):
 	if len(argv) > 1:
-		if argv[1] == 'vertex' or argv[1] == 'v':
+		if argv[1] == 'vertex' or argv[1] == 'v' or \
+		   argv[1] == 'edge' or argv[1] == 'e':
 			if gno < 0 or cno < 0:
 				print('Component does not exist')
 				return;
 
-			schema_command_vertex(argv, gno, cno)
+			schema_command_add(argv, gno, cno)
 			return
 
 	if gno >= 0 and cno >= 0:
