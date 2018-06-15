0cf8aaee123444e909ce5c8f17fbd8ccc0d97bb1
Frank W. Miller <fwmiller@ubuntu1804lts.localdomain>
2018-06-15 14:18:47 +0000

    Coded tuple.dump()

--- tuple.py
+++ tuple.py
@@ -17,46 +17,47 @@ def dump(s, fd):
 			u = int.from_bytes(8, byteorder='little', signed=False)
 			print(u, end='')
 
-elif attrtype == 'FLOAT':
-b = fd1.read(4)
-if not b:
-b = bytearray(struct.pack('f', 0.0))
-fd2.write(b)
+		elif attrtype == 'FLOAT':
+			b = fd1.read(4)
+			if not b:
+				break
+			f = struct.unpack('f', b)
+			print(f, end='')
 
-elif attrtype == 'DOUBLE':
-b = fd1.read(8)
-if not b:
-b = bytearray(struct.pack('d', 0.0))
-fd2.write(b)
+		elif attrtype == 'DOUBLE':
+			b = fd1.read(8)
+			if not b:
+				break
+			print(d, end='')
 
-elif attrtype == 'CHAR':
-b = fd1.read(1)
-if not b:
-b = (0).to_bytes(1, byteorder='little')
-fd2.write(b)
+		elif attrtype == 'CHAR':
+			b = fd1.read(1)
+			if not b:
+				break
+			s = str(b)
+			print(s, end='')
 
-elif attrtype == 'STRING':
-b = fd1.read(2)
-if not b:
-b = (0).to_bytes(2, byteorder='little', signed=False)
-fd2.write(b)
-else:
-fd2.write(b)
-len = int.from_bytes(b, byteorder='little', signed=False)
-b = fd1.read(len)
-if b:
-fd2.write(b)
+		elif attrtype == 'STRING':
+			b = fd1.read(2)
+			len = struct.unpack('<H', b)
+			b = fd1.read(len)
+			if not b:
+				break
+			s = str(b)
+			print(s, end='')
 
-elif attrtype == 'DATE':
-b = fd1.read(10)
-if not b:
-b = (0).to_bytes(10, byteorder='little')
-fd2.write(b)
+		elif attrtype == 'DATE':
+			b = fd1.read(10)
+			if not b:
+				break
+			s = str(b)
+			print(s, end='')
 
-elif attrtype == 'TIME':
-b = fd1.read(8)
-if not b:
-b = (0).to_bytes(8, byteorder='little')
-fd2.write(b)
+		elif attrtype == 'TIME':
+			b = fd1.read(8)
+			if not b:
+				break
+			s = str(b)
+			print(s, end='')
 
     print(']', end='')
